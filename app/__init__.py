1c6da71a077dc59a420611817a996028929b24e4
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-05-08 15:54:50 +0300

    init.py:it will initialize the app

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,34 @@
-from flask import Flask
+from flask import Flask 
+from flask_mail import Mail
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from flask_uploads import IMAGES, UploadSet,configure_uploads
 
-def create_app():
-  app =Flask (__name__)
+db = SQLAlchemy()
+mail = Mail()
+# bootstap = Bootstrap()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+photos = UploadSet('photos',IMAGES)
 
-  app.config['SECRET_KEY']='1234567890'
 
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config_options[config_name])
+    from .auth import auth as authentication_blueprint
+    from .main import main as main_blueprint
+
+    app.register_blueprint(authentication_blueprint)
+    app.register_blueprint(main_blueprint)
+
+    login_manager.init_app(app)
+    db.init_app(app)
+    # bootstap.init_app(app)
+    configure_uploads(app,photos)
+    mail.init_app(app)
+    
+
+
+    return app
