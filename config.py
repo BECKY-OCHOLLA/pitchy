fbb574ba142beb2d99caeb7418558ec2acadf7b5
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-05-08 16:22:40 +0300

    it will configuire sqlalchemy uri

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+
+
+
+
+class Config:
+    debug = True
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://cate:love1234@localhost/love'
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    
