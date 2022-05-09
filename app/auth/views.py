0bf81737ebb589884bec2222ff25fae2b7cf73c4
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-05-09 14:02:23 +0300

    user class:it will add a user class and tables and columns for it

--- app/auth/views.py
+++ app/auth/views.py
@@ -25,3 +25,44 @@ def login():
         login_user(user)
         return redirect('/')
     return render_template('login.html')
+
+
+@auth.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        form = request.form
+        username = form.get("username")
+        email = form.get("email")
+        password = form.get("password")
+        confirm_password = form.get("confirm_password")
+        if username is None or password is None or email is None or confirm_password is None:
+            error = 'username, email, password are required'
+            return render_template('signup.html', error=error)
+        if ' ' in username:
+            error = 'Username should not contain spaces'
+            return render_template('signup.html', error=error)
+        if password != confirm_password:
+            error = "Passwords do not match"
+            return render_template('signup.html', error=error)
+        else:
+            user = User.query.filter_by(username=username).first()
+            if user is not None:
+                error = 'A user with that name already exists'
+                return render_template('signup.html', error=error)
+            user = User.query.filter_by(email=email).first()
+            if user is not None:
+                error = 'A user with that email already exists'
+                return render_template('signup.html', error=error)
+            user = User(username=username, email=email)
+            user.set_password(password)
+            user.save()
+            # email_message("Welcome to 1m Pitch", "email/howdy", user.email, user=user)
+            return redirect(url_for('auth.login'))
+
+    return render_template('signup.html')
+
+
+@auth.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
