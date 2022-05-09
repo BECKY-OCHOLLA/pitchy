2ebcf17f70d5ed45918c01d1c45dba5d7899d198
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-05-09 11:38:25 +0300

    it will add the post routing

--- app/main/views.py
+++ app/main/views.py
@@ -2,7 +2,7 @@ from flask import render_template, redirect, url_for
 from flask_login import login_required, current_user
 
 from . import main
-from .forms import PostForm, CommentForm, UpdateProfile
+# from .forms import PostForm, CommentForm, UpdateProfile
 from ..models import Post, Comment, User, Upvote, Downvote
 
 @main.route('/')
@@ -12,3 +12,13 @@ def index():
     idea = Post.query.filter_by(category='idea').all()
     business = Post.query.filter_by(category='Business').all()
     return render_template('index.html', business=business, product=product, idea=idea, posts=posts)
+
+@main.route('/posts')
+@login_required
+def posts():
+    posts = Post.query.all()
+    likes = Upvote.query.all()
+    user = current_user
+    return render_template('pitch_display.html', posts=posts, likes=likes, user=user)
+
+
